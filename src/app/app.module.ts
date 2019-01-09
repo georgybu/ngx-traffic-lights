ffd68fd0edebebd8fe9accbaa51e481d5beb1a27
BrkCoder <baribarakinbal@gmail.com>
2019-01-09 08:17:59 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AppComponent } from './app.component';
+import { TrafficLightsComponent } from './featured/traffic-lights/traffic-lights.component';
+import { reducers, metaReducers } from './store';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrafficLightsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/featured/traffic-lights/traffic-lights.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-traffic-lights',
+  templateUrl: './traffic-lights.component.html',
+  styleUrls: ['./traffic-lights.component.scss']
+})
+export class TrafficLightsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
